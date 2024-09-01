5b4ad5be260df631b5f65cdd690c5d7b3710cecd
glomo <badjokeresult@gmail.com>
2024-09-01 14:14:23 +0300

    new-outer-api

--- leafcore/src/lib.rs
+++ leafcore/src/lib.rs
@@ -1,43 +1,19 @@
-mod shared_secret;
+use lazy_static::lazy_static;
+
+mod codec;
 mod crypto;
+mod hash;
+mod messages;
+mod peer;
+mod shared_secret;
+mod storage;
 
-use std::str::FromStr;
+use async_once::AsyncOnce;
+use crate::peer::server::BroadcastServerPeer;
 
-use shared_secret::{ShamirSecretSharer, SecretSharer};
-use crypto::{Aes256Encryptor, Encryptor};
-
-use leafpeer;
-
-pub async fn init() -> (Aes256Encryptor, ShamirSecretSharer) {
-    let encryptor = Aes256Encryptor::new().await;
-    let sharer = ShamirSecretSharer::new(5);
-    leafpeer::init().await;
-    (encryptor, sharer)
+lazy_static! {
+    static ref PEER_SERVER: AsyncOnce<BroadcastServerPeer> = AsyncOnce::new(async {
+        BroadcastServerPeer::new(62092).await
+    });
 }
 
-pub async fn store_file(content: Vec<u8>, sharer: &ShamirSecretSharer, encryptor: &mut Aes256Encryptor) -> Vec<Vec<u8>> {
-    let chunks = sharer.split_into_chunks(content);
-
-    let mut encrypted_chunks = vec![];
-    for chunk in &chunks {
-        encrypted_chunks.push(encryptor.encrypt_chunk(chunk));
-    }
-
-    let sent_hashes = leafpeer::send_chunks_to_peers(encrypted_chunks).await;
-    sent_hashes
-}
-
-pub async fn receive_file(hashes: Vec<Vec<u8>>, sharer: &ShamirSecretSharer, encryptor: &mut Aes256Encryptor) -> Vec<u8> {
-    let encrypted_chunks = leafpeer::receive_chunks_from_peers(hashes).await;
-    let mut chunks = vec![];
-    for chunk in &encrypted_chunks {
-        chunks.push(encryptor.decrypt_chunk(chunk));
-    }
-
-    let content = sharer.recover_from_chunks(chunks);
-    content
-}
-
-pub async fn shutdown() {
-    leafpeer::shutdown().await;
-}
