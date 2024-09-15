459149b6f13f169c9c32c564bf3c999c49282d12
glomo <badjokeresult@gmail.com>
2024-09-15 15:19:27 +0300

    codec-impovements

--- leafcore/src/codec/codec.rs
+++ leafcore/src/codec/codec.rs
@@ -1,6 +1,6 @@
-use crate::messages::types::MessageType;
+use crate::messages::types::Message;
 
 pub trait Codec {
-    fn encode_message(message: MessageType) -> Vec<u8>;
-    fn decode_message(message: &[u8]) -> MessageType;
+    fn encode_message(message: Message) -> Vec<u8>;
+    fn decode_message(message: &[u8]) -> Message;
 }
