fc772329b31c157e6aabab579556222d3fdfb32a
glomo <badjokeresult@gmail.com>
2024-10-06 16:19:30 +0300

    debug

--- leaf-test-client/src/main.rs
+++ leaf-test-client/src/main.rs
@@ -4,6 +4,7 @@ use leaf_client::*;
 #[tokio::main]
 async fn main() {
     let content = fs::read("test.txt").unwrap();
+    println!("LEN CONTENT : {}", content.len());
     let hashes = send_file(content).await;
     println!("HASHES COUNT : {}", hashes.len());
 }
