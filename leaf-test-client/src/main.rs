e75bce9e2f9d9cb2d5b4096e7af8904ef92d9017
glomo <badjokeresult@gmail.com>
2024-10-08 17:27:17 +0300

    remove-test-proj

--- leaf-test-client/src/main.rs
+++
@@ -1,8 +0,0 @@
-use tokio::net::UdpSocket;
-
-#[tokio::main]
-async fn main() {
-    let socket = UdpSocket::bind("192.168.124.1:62092").await.unwrap();
-    socket.set_broadcast(true).unwrap();
-    socket.send_to(b"Hello world", "192.168.124.255:62092").await.unwrap();
-}
