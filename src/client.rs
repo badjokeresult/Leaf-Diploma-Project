f5b9fb7f7e16319375c6b2f24ed9be844e6b9d30
glomo <badjokeresult@gmail.com>
2024-10-25 14:40:56 +0300

    unrefactored-client

--- src/client.rs
+++ src/client.rs
@@ -1,23 +1,63 @@
 use std::io::Error;
 use std::net::SocketAddr;
-use std::sync::mpsc;
-use std::sync::mpsc::{Receiver, Sender};
-use tools::Message;
-use crate::peer::Peer;
+use std::sync::mpsc::Receiver;
+use crate::message::{Message, consts::*};
+use crate::peer::BroadcastUdpPeer;
 
-pub struct Client {
-    peer: Peer,
+pub struct BroadcastUdpClient {
+    peer: BroadcastUdpPeer,
+    from_peer_receiver: Receiver<(Message, SocketAddr)>,
 }
 
-impl Client {
-    pub fn new(addr: SocketAddr) -> Client {
-        let peer = Peer::new(addr);
-        Client { peer }
+impl BroadcastUdpClient {
+    pub fn new() -> BroadcastUdpClient {
+        let (peer, from_peer_receiver) = BroadcastUdpPeer::new().unwrap();
+
+        BroadcastUdpClient {
+            peer,
+            from_peer_receiver,
+        }
     }
-}
 
-impl Client {
-    pub fn send_content(&self, content: &[u8]) {
+    pub fn send(&self, hash: &[u8], chunk: &[u8]) -> Result<(), Error> {
+        let message = Message::new(SENDING_REQUEST_TYPE, hash);
+        self.peer.send_req(Into::<Vec<_>>::into(message).as_slice())?;
+        loop {
+            if let Ok((m, a)) = self.from_peer_receiver.recv() {
+                if let Message::SendingAck(h) = m {
+                    if h == hash {
+                        let messages = Message::new_with_data(CONTENT_FILLED_TYPE, hash, chunk.to_vec());
+                        for msg in messages {
+                            self.peer.send_content(&Into::<Vec<_>>::into(msg), a)?;
+                        }
+                        return Ok(());
+                    }
+                }
+            }
+        };
+    }
+
+    pub fn recv(&self, hash: &[u8]) -> Result<Vec<u8>, Error> {
+        let mut result = vec![];
+
+        let message = Message::new(RETRIEVING_REQUEST_TYPE, hash);
+        self.peer.send_req(Into::<Vec<_>>::into(message).as_slice())?;
+        loop {
+            if let Ok((m, _)) = self.from_peer_receiver.recv() {
+                if let Message::RetrievingAck(h, mut d) = m {
+                    if h == hash {
+                        result.append(&mut d);
+                    }
+                } else if let Message::Empty(h) = m {
+                    if h == hash {
+                        return Ok(result);
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn shutdown(&self) {
 
     }
 }
