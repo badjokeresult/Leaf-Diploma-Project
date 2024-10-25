916d0fe1b250a3fa278a49b0bb44a39a6043067f
glomo <badjokeresult@gmail.com>
2024-10-25 14:37:04 +0300

    test-app

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-mod peer;
+mod lib;
+
+use std::fs;
+use lib::*;
 
 fn main() {
-    println!("Hello, world!");
+    let client_ptr = init();
+    let content = fs::read("test.txt").unwrap();
+    let hashes = send_file(content, client_ptr);
+    let new_content = recv_content(hashes, client_ptr);
+    fs::write("test1.txt", &new_content).unwrap();
 }
+
