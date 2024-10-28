18379b2abcdc67cac6d9bca8af680d6807e27e8e
glomo <badjokeresult@gmail.com>
2024-10-28 15:11:36 +0300

    non-atomic-clone

--- src/peer.rs
+++ src/peer.rs
@@ -1,6 +1,6 @@
 use std::io::Error;
 use std::net::{IpAddr, SocketAddr, UdpSocket};
-use std::sync::{mpsc, Arc, Mutex, mpsc::{Receiver, Sender}};
+use std::sync::{mpsc, mpsc::{Receiver, Sender}};
 use std::thread::{JoinHandle, spawn};
 use std::time::Duration;
 use net2::UdpBuilder;
@@ -10,7 +10,7 @@ use crate::server::BroadcastUdpServer;
 
 pub struct BroadcastUdpPeer {
     socket: UdpSocket,
-    server: Arc<Mutex<BroadcastUdpServer>>,
+    server: BroadcastUdpServer,
     to_client_sender: Sender<(Message, SocketAddr)>,
     broadcast_addr: IpAddr,
 }
@@ -24,7 +24,7 @@ impl BroadcastUdpPeer {
         socket.set_broadcast(true)?;
         socket.set_read_timeout(Some(Duration::new(5, 0)))?;
         socket.set_write_timeout(Some(Duration::new(3, 0)))?;
-        let server = Arc::new(Mutex::new(BroadcastUdpServer::new()));
+        let server = BroadcastUdpServer::new();
         let (to_client_sender, to_client_receiver) = mpsc::channel::<(Message, SocketAddr)>();
 
         Ok((BroadcastUdpPeer {
@@ -39,7 +39,7 @@ impl BroadcastUdpPeer {
         let mut handles = vec![];
 
         for _ in 0..num_threads {
-            let server = Arc::clone(&self.server);
+            let server = self.server.clone();
             let socket = self.socket.try_clone().unwrap();
             let sender = self.to_client_sender.clone();
             let mut buf = [0u8; MAX_DATAGRAM_SIZE];
@@ -51,16 +51,16 @@ impl BroadcastUdpPeer {
                             let message = Message::from(buf[..s].to_vec());
                             match message {
                                 Message::SendingReq(h) => {
-                                    let answer = server.lock().unwrap().handle_sending_req(&h).unwrap();
+                                    let answer = server.handle_sending_req(&h).unwrap();
                                     socket.send_to(Into::<Vec<_>>::into(answer).as_slice(), a).unwrap();
                                 },
                                 Message::RetrievingReq(h) => {
-                                    let chunks = server.lock().unwrap().handle_retrieving_req(&h).unwrap();
+                                    let chunks = server.handle_retrieving_req(&h).unwrap();
                                     for chunk in chunks {
                                         socket.send_to(Into::<Vec<_>>::into(chunk).as_slice(), a).unwrap();
                                     }
                                 },
-                                Message::ContentFilled(h, d) => match server.lock().unwrap().handle_content_filled(&h, &d) {
+                                Message::ContentFilled(h, d) => match server.handle_content_filled(&h, &d) {
                                     Ok(_) => {},
                                     Err(e) => eprintln!("{}", e.to_string()),
                                 },
