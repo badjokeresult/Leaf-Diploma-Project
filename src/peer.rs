cb4a2c5395b5aafc012bd87ec3a2e17b7cbe23f2
glomo <badjokeresult@gmail.com>
2024-10-28 14:51:24 +0300

    no-atomic

--- src/peer.rs
+++ src/peer.rs
@@ -9,7 +9,7 @@ use crate::message::Message;
 use crate::server::BroadcastUdpServer;
 
 pub struct BroadcastUdpPeer {
-    socket: Arc<Mutex<UdpSocket>>,
+    socket: UdpSocket,
     server: Arc<Mutex<BroadcastUdpServer>>,
     to_client_sender: Sender<(Message, SocketAddr)>,
     broadcast_addr: IpAddr,
@@ -20,10 +20,10 @@ const MAX_DATAGRAM_SIZE: usize = 65_507;
 impl BroadcastUdpPeer {
     pub fn new(local_ip: IpAddr, local_broadcast: IpAddr) -> Result<(BroadcastUdpPeer, Receiver<(Message, SocketAddr)>), Error> {
         let addr = SocketAddr::new(local_ip, 62092);
-        let socket = Arc::new(Mutex::new(UdpBuilder::new_v4()?.reuse_address(true)?.reuse_port(true)?.bind(addr)?));
-        socket.lock().unwrap().set_broadcast(true)?;
-        socket.lock().unwrap().set_read_timeout(Some(Duration::new(5, 0)))?;
-        socket.lock().unwrap().set_write_timeout(Some(Duration::new(3, 0)))?;
+        let socket = UdpBuilder::new_v4()?.reuse_address(true)?.reuse_port(true)?.bind(addr)?;
+        socket.set_broadcast(true)?;
+        socket.set_read_timeout(Some(Duration::new(5, 0)))?;
+        socket.set_write_timeout(Some(Duration::new(3, 0)))?;
         let server = Arc::new(Mutex::new(BroadcastUdpServer::new()));
         let (to_client_sender, to_client_receiver) = mpsc::channel::<(Message, SocketAddr)>();
 
@@ -40,24 +40,24 @@ impl BroadcastUdpPeer {
 
         for _ in 0..num_threads {
             let server = Arc::clone(&self.server);
-            let socket = Arc::clone(&self.socket);
+            let socket = self.socket.try_clone().unwrap();
             let sender = self.to_client_sender.clone();
             let mut buf = [0u8; MAX_DATAGRAM_SIZE];
 
             let handle = spawn(move || {
                 loop {
-                    match socket.lock().unwrap().recv_from(&mut buf) {
+                    match socket.recv_from(&mut buf) {
                         Ok((s, a)) => {
                             let message = Message::from(buf[..s].to_vec());
                             match message {
                                 Message::SendingReq(h) => {
                                     let answer = server.lock().unwrap().handle_sending_req(&h).unwrap();
-                                    socket.lock().unwrap().send_to(Into::<Vec<_>>::into(answer).as_slice(), a).unwrap();
+                                    socket.send_to(Into::<Vec<_>>::into(answer).as_slice(), a).unwrap();
                                 },
                                 Message::RetrievingReq(h) => {
                                     let chunks = server.lock().unwrap().handle_retrieving_req(&h).unwrap();
                                     for chunk in chunks {
-                                        socket.lock().unwrap().send_to(Into::<Vec<_>>::into(chunk).as_slice(), a).unwrap();
+                                        socket.send_to(Into::<Vec<_>>::into(chunk).as_slice(), a).unwrap();
                                     }
                                 },
                                 Message::ContentFilled(h, d) => match server.lock().unwrap().handle_content_filled(&h, &d) {
@@ -78,12 +78,12 @@ impl BroadcastUdpPeer {
 
     pub fn send_req(&self, data: &[u8]) -> Result<(), Error> {
         let broadcast = SocketAddr::new(self.broadcast_addr, 62092);
-        self.socket.lock().unwrap().send_to(data, broadcast)?;
+        self.socket.send_to(data, broadcast)?;
         Ok(())
     }
 
     pub fn send_content(&self, data: &[u8], addr: SocketAddr) -> Result<(), Error> {
-        self.socket.lock().unwrap().send_to(data, addr)?;
+        self.socket.send_to(data, addr)?;
         Ok(())
     }
 }
