c1f39d686721dcb8d1b5023c2b8fb9c97a42c714
glomonosov <badjokeresult@gmail.com>
2024-11-05 15:13:53 +0300

    saving-on-disk-in-time

--- leaflibrary/src/storage.rs
+++ leaflibrary/src/storage.rs
@@ -1,82 +1,65 @@
+use std::collections::HashMap;
+use std::fs::OpenOptions;
 use std::path::PathBuf;
-use std::sync::Arc;
 
-use tokio::sync::Mutex;
-use serde::{Deserialize, Serialize};
 use tokio::fs;
 use uuid::Uuid;
-
-#[derive(Serialize, Deserialize, Eq, PartialEq)]
-struct FileChunk {
-    pub hash: Vec<u8>,
-    data: Vec<u8>,
-    pub full_flag: bool,
-}
-
-impl FileChunk {
-    fn new(hash: Vec<u8>, data: Vec<u8>) -> Self {
-        Self { hash, data, full_flag: false, }
-    }
-}
 
 #[derive(Clone)]
 pub struct BroadcastUdpServerStorage {
     storage_path: PathBuf,
-    curr_chunks: Arc<Mutex<Vec<FileChunk>>>,
+    database: HashMap<Vec<u8>, String>,
 }
 
 impl BroadcastUdpServerStorage {
-    pub fn new(storage_path: &PathBuf) -> Self {
+    pub async fn new(storage_path: &PathBuf) -> Self {
         Self {
             storage_path: storage_path.clone(),
-            curr_chunks: Arc::new(Mutex::new(Vec::new())),
+            database: Self::from_file(storage_path).await.unwrap_or_else(|e| HashMap::new()),
+        }
+    }
+
+    async fn from_file(storage_path: &PathBuf) -> Result<HashMap<Vec<u8>, String>, tokio::io::Error> {
+        match fs::read(match storage_path.parent() {
+            Some(p) => p.join("db.bin"),
+            None => return Err(tokio::io::Error::last_os_error()),
+        }).await {
+            Ok(data) => Ok(serde_json::from_slice(&data)?),
+            Err(_) => Err(tokio::io::Error::last_os_error()),
         }
     }
 
     pub async fn add(&mut self, hash: &[u8], data: &[u8]) -> Result<(), tokio::io::Error> {
-        self.curr_chunks.lock().await.push(FileChunk::new(hash.to_vec(), data.to_vec()));
+        for (h, s) in &self.database {
+            if hash.eq(h) {
+                let mut f = OpenOptions::new()
+                    .append(true)
+                    .open(s).await?;
+                f.write(data).await?;
+                return Ok(());
+            }
+        }
+
+        let filename = Uuid::new_v4().to_string() + ".dat";
+        let filepath = self.storage_path.join(filename).to_str().unwrap().to_string();
+        match fs::write(&filepath, data).await {
+            Ok(_) => self.database.insert(hash.to_vec(), filepath).unwrap(),
+            Err(e) => return Err(tokio::io::Error::new(tokio::io::ErrorKind::Other, format!("{:?}", e))),
+        };
         Ok(())
     }
 
     pub async fn retrieve(&self, hash: &[u8]) -> Result<Vec<u8>, tokio::io::Error> {
-        let files = std::fs::read_dir(&self.storage_path)?;
-        for file in files {
-            let filename = file?.path();
-            let content: FileChunk = serde_json::from_slice(&fs::read(&filename).await?)?;
-            if content.hash.eq(hash) {
-                fs::remove_file(filename).await?;
-                return Ok(content.data);
+        for (h, s) in &self.database {
+            if hash.eq(h) {
+                return Ok(fs::read(s).await?);
             }
         }
         Err(tokio::io::Error::last_os_error())
     }
 
-    pub async fn finalize(&mut self, hash: &[u8]) -> Result<(), tokio::io::Error> {
-        println!("LEN OF CURR CHUNKS : {}", self.curr_chunks.lock().await.len());
-        let mut chunks = self.curr_chunks.lock().await;
-        for i in 0..chunks.len() {
-            if chunks[i].hash.eq(hash) && !chunks[i].full_flag {
-                chunks[i].full_flag = true;
-                self.save_chunk(&chunks[i]).await?;
-            }
-        }
-        Ok(())
-    }
-
-    async fn save_chunk(&self, chunk: &FileChunk) -> Result<(), tokio::io::Error> {
-        let json = serde_json::to_vec(&chunk)?;
-        let filepath = self.storage_path.join(Uuid::new_v4().to_string() + ".dat");
-        fs::write(&filepath, &json).await?;
-        Ok(())
-    }
-
     pub async fn shutdown(&self) {
-        let mut chunks = self.curr_chunks.lock().await;
-        for i in 0..chunks.len() {
-            if chunks[i].full_flag {
-                self.save_chunk(&chunks[i]).await.unwrap();
-                chunks.remove(i);
-            }
-        };
+        let json = serde_json::to_vec(&self.database).unwrap();
+        fs::write(&self.storage_path.parent().unwrap().join("db.bin"), json).await.unwrap();
     }
 }
