5c426c6d67cf1af730e243e961b21bd495fdd358
badjokeresult <badjokeresult@gmail.com>
2025-01-19 11:26:36 +0300

    socket-mod-provided

---
+++ server/src/socket.rs
@@ -0,0 +1,62 @@
+use std::net::SocketAddr;
+use std::sync::Arc;
+use std::time::Duration;
+
+use tokio::net::UdpSocket;
+use tokio::sync::mpsc;
+use tokio::time;
+
+pub struct Packet {
+    pub data: Vec<u8>,
+    pub addr: SocketAddr,
+}
+
+impl Packet {
+    pub fn new(data: Vec<u8>, addr: SocketAddr) -> Packet {
+        Packet { data, addr }
+    }
+
+    pub fn deconstruct(self) -> (Vec<u8>, SocketAddr) {
+        (self.data, self.addr)
+    }
+}
+
+#[derive(Clone)]
+pub struct Socket {
+    socket: Arc<UdpSocket>,
+    sender: mpsc::Sender<Packet>,
+}
+
+impl Socket {
+    pub async fn new() -> (Socket, mpsc::Receiver<Packet>) {
+        let socket = Arc::new(UdpSocket::bind("0.0.0.0:62092").await.unwrap());
+        socket.set_broadcast(true).unwrap();
+
+        let (tx, rx) = mpsc::channel(100).await;
+        (Socket {
+            socket,
+            sender: tx,
+        }, rx)
+    }
+
+    pub async fn send(&self, mut rx: mpsc::Receiver<Packet>) {
+        loop {
+            time::sleep(Duration::from_millis(100)).await;
+            if let Some(p) = rx.recv().await {
+                let (data, addr) = p.deconstruct();
+                self.socket.send_to(&data, addr).await.unwrap();
+            }
+        }
+    }
+
+    pub async fn recv(&self) {
+        let mut buf = [0u8; 4096];
+        loop {
+            time::sleep(Duration::from_millis(100)).await;
+            if let Ok((s, a)) = self.socket.recv_from(&mut buf).await {
+                let packet = Packet::new(buf[..s].to_vec(), a);
+                self.sender.send(packet).await.unwrap();
+            }
+        }
+    }
+}
