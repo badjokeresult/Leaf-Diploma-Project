7e80085a9ec2aec69fbbf62806cf21964e0c20b4
badjokeresult <badjokeresult@gmail.com>
2025-01-20 15:09:46 +0300

    broadcast-channel

--- server/src/socket.rs
+++ server/src/socket.rs
@@ -3,9 +3,10 @@ use std::sync::Arc;
 use std::time::Duration;
 
 use tokio::net::UdpSocket;
-use tokio::sync::mpsc;
+use tokio::sync::broadcast;
 use tokio::time;
 
+#[derive(Clone, Debug)]
 pub struct Packet {
     pub data: Vec<u8>,
     pub addr: SocketAddr,
@@ -24,19 +25,20 @@ impl Packet {
 #[derive(Clone)]
 pub struct Socket {
     socket: Arc<UdpSocket>,
-    sender: mpsc::Sender<Packet>,
+    sender: broadcast::Sender<Packet>,
 }
 
 impl Socket {
-    pub async fn new() -> (Socket, mpsc::Receiver<Packet>) {
+    pub async fn new() -> (Socket, broadcast::Sender<Packet>) {
         let socket = Arc::new(UdpSocket::bind("0.0.0.0:62092").await.unwrap());
         socket.set_broadcast(true).unwrap();
 
-        let (tx, rx) = mpsc::channel(100).await;
+        let (tx, _) = broadcast::channel(100);
+
         (Socket {
             socket,
-            sender: tx,
-        }, rx)
+            sender: tx.clone(),
+        }, tx)
     }
 
     pub async fn send(&self, packet: Packet) {
@@ -50,7 +52,7 @@ impl Socket {
             time::sleep(Duration::from_millis(100)).await;
             if let Ok((s, a)) = self.socket.recv_from(&mut buf).await {
                 let packet = Packet::new(buf[..s].to_vec(), a);
-                self.sender.send(packet).await.unwrap();
+                self.sender.send(packet).unwrap();
             }
         }
     }
