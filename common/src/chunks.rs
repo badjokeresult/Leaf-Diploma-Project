61050cab8f366443f0962a132e983a74155e4222
badjokeresult <badjokeresult@gmail.com>
2025-02-11 12:06:26 +0300

    fix-mismatch

--- common/src/chunks.rs
+++ common/src/chunks.rs
@@ -72,7 +72,7 @@ impl SecretSharer for ReedSolomonSecretSharer {
         // Метод разбиения файла на блоки
         let block_size = self.calc_block_size(secret.len()); // Получение размера блока
         let amount_of_blocks = Self::calc_amount_of_blocks(secret.len(), block_size); // Получение количества блоков
-        let mut buf = vec![0u8; block_size * amount_of_blocks]; // Создание буфера для хранения блоков
+        let mut buf = vec![]; // Создание буфера для хранения блоков
         for i in secret {
             buf.push(*i); // Перемещение байтов файла в буфер
         }
