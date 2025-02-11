996f995bfaa9b9a38e1b4eee9ecb1fbf689bde07
badjokeresult <badjokeresult@gmail.com>
2025-02-11 16:32:38 +0300

    proper-while

--- server/src/socket.rs
+++ server/src/socket.rs
@@ -1,10 +1,8 @@
 use std::net::SocketAddr; // Зависимость стандартной библиотеки для работы с сетевыми адресами
 use std::sync::Arc; // Зависимость стандартной библиотеки для работы с объектами в многопоточном режиме
-use std::time::Duration; // Зависимость стандартной библиотеки для обеспечения простоя потока
 
 use tokio::net::UdpSocket; // Внешняя зависимость для работы с асинхронным UDP-сокетом
 use tokio::sync::broadcast; // Внешняя зависимость для работы с асинхронными широковещательными каналами
-use tokio::time; // Внешняя зависимость для работы со временем в асинхронном исполнении
 
 use consts::*; // Внутренняя зависимость для использования констант
 
@@ -13,7 +11,6 @@ mod consts {
     pub const LOCAL_ADDR: &str = "0.0.0.0:62092"; // Строка адреса для открытия сокета
     pub const CHAN_QUEUE_SIZE: usize = 100; // Размер очереди для канала
     pub const UDP_SOCKET_BUF_SIZE: usize = 65535; // Размер буфера для приема данных из сети
-    pub const TIMEOUT_MILLIS: u64 = 100; // Значение таймаута для переключения потока
 }
 
 #[derive(Clone, Debug)]
@@ -66,13 +63,10 @@ impl Socket {
     pub async fn recv(&self) {
         // Метод получения данных из сети
         let mut buf = [0u8; UDP_SOCKET_BUF_SIZE]; // Создаем буфер
-        loop {
-            time::sleep(Duration::from_millis(TIMEOUT_MILLIS)).await; // Задержка для переключения потока
-            if let Ok((s, a)) = self.socket.recv_from(&mut buf).await {
-                // Если в сокете есть данные
-                let packet = Packet::new(buf[..s].to_vec(), a); // Собираем из данных пакет
-                self.sender.send(packet).unwrap(); // Отправляем пакет по каналу получателям для дальнейшей обработки
-            }
+        while let Ok((s, a)) = self.socket.recv_from(&mut buf).await {
+            // Если в сокете есть данные
+            let packet = Packet::new(buf[..s].to_vec(), a); // Собираем из данных пакет
+            self.sender.send(packet).unwrap(); // Отправляем пакет по каналу получателям для дальнейшей обработки
         }
     }
 }
