b65491e7aba3fc59fba485542b8b5f3ea3cba7d0
badjokeresult <badjokeresult@gmail.com>
2025-03-28 10:57:58 +0300

    reed-solomon-interface-for-outer-usage-other-and-messages-other-is-private

--- leafclient/src/main.rs
+++ leafclient/src/main.rs
@@ -1,20 +1,10 @@
-#![allow(unused_variables)] // Директива для игнорирования неиспользуемых переменных функций (переменная пароля не используется для смены пользователя в Linux, но она нужна для поддержания единого интерфейса)
-
 use std::path::{Path, PathBuf}; // Зависимость стандартной библиотеки для работы с файловыми путями
 
 use clap::Parser; // Внешние зависимости для работы с аргументами командной строки
 use clap::{arg, command};
 use clap_derive::{Parser, ValueEnum};
 
-use leafcommon::{
-    // Зависимости внутренней библиотеки проекта
-    Chunks,
-    ChunksHashes,
-    Encryptor,
-    KuznechikEncryptor,
-    ReedSolomonChunks,
-    ReedSolomonChunksHashes,
-};
+use leafcommon;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -54,53 +44,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = load_args(); // Получение аргументов командной строки
 
     // Используем тот же пароль для шифрования шифровальщика
-    let encryptor: Box<dyn Encryptor> = Box::new(KuznechikEncryptor::new().await?);
-
     let path = &args.file;
     match args.get_action() {
-        Action::Send => send_file(path, encryptor).await,
-        Action::Receive => recv_file(path, encryptor).await, // Если получение - вызываем функцию получения
+        Action::Send => send_file(path).await,
+        Action::Receive => recv_file(path).await, // Если получение - вызываем функцию получения
     }
 }
 
-async fn send_file(
-    path: impl AsRef<Path>,
-    encryptor: Box<dyn Encryptor>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    // Функция отправки файла
-    let mut chunks = ReedSolomonChunks::from_file(&path).await?; // Получаем чанки
-    chunks.encrypt(&encryptor)?; // Шифруем их
-    chunks.update_hashes()?; // Обновляем их хэш-суммы
-    let hashes = chunks.send().await?; // Отправляем чанки в домен и получаем назад их хэш-суммы
-    hashes.save_to(path).await?; // Сохраняем хэш-суммы в целевом файле
-    Ok(())
+async fn send_file(path: impl AsRef<Path>) -> Result<(), Box<dyn std::error::Error>> {
+    leafcommon::reed_solomon_scheme::send_file(path).await
 }
 
-async fn recv_file(
-    path: impl AsRef<Path>,
-    encryptor: Box<dyn Encryptor>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    // Функция получения файла
-    let hashes = ReedSolomonChunksHashes::load_from(&path).await?; // Получаем хэш-суммы из файла
-    let mut chunks = ReedSolomonChunks::recv(hashes).await?; // Получаем чанки по хэшам
-    chunks.decrypt(&encryptor)?; // Расшифровываем чанки
-    chunks.into_file(path).await?; // Восстанавливаем из них содержимое и записываем его в целевой файл
-    Ok(())
-}
-
-mod errors {
-    // Модуль с составными типами ошибок
-    use std::error::Error; // Трейт ошибки из стандартной библиотеки
-    use std::fmt::{Display, Formatter}; // Зависимости стандартной библиотеки для отображения данных на экране
-
-    #[derive(Debug, Clone)]
-    pub struct SwitchUserError; // Ошибка смены пользователя
-
-    impl Display for SwitchUserError {
-        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-            write!(f, "Error switching user or group") // Текст ошибки смены пользователя
-        }
-    }
-
-    impl Error for SwitchUserError {} // Реализация ошибки по умолчанию
+async fn recv_file(path: impl AsRef<Path>) -> Result<(), Box<dyn std::error::Error>> {
+    leafcommon::reed_solomon_scheme::recv_file(path).await
 }
--- leafcommon/src/lib.rs
+++ leafcommon/src/lib.rs
@@ -1,11 +1,36 @@
-pub mod crypto;
-pub use crypto::{Encryptor, KuznechikEncryptor};
 pub mod message;
 pub use message::Message;
+
+mod crypto;
 mod shards;
 
-pub mod chunks;
-pub use chunks::{
-    Chunk, ChunkHash, Chunks, ChunksHashes, ReedSolomonChunk, ReedSolomonChunkHash,
-    ReedSolomonChunks, ReedSolomonChunksHashes,
-};
+mod chunks;
+
+pub mod reed_solomon_scheme {
+    use super::chunks::{Chunks, ChunksHashes, ReedSolomonChunks, ReedSolomonChunksHashes};
+    use super::crypto::{Encryptor, KuznechikEncryptor};
+
+    use std::error::Error;
+    use std::path::Path;
+
+    pub async fn send_file(path: impl AsRef<Path>) -> Result<(), Box<dyn Error>> {
+        let encryptor: Box<dyn Encryptor> = Box::new(KuznechikEncryptor::new().await?);
+
+        let mut chunks = ReedSolomonChunks::from_file(&path).await?; // Получаем чанки
+        chunks.encrypt(&encryptor)?; // Шифруем их
+        chunks.update_hashes()?; // Обновляем их хэш-суммы
+        let hashes = chunks.send().await?; // Отправляем чанки в домен и получаем назад их хэш-суммы
+        hashes.save_to(path).await?; // Сохраняем хэш-суммы в целевом файле
+        Ok(())
+    }
+
+    pub async fn recv_file(path: impl AsRef<Path>) -> Result<(), Box<dyn Error>> {
+        let decryptor: Box<dyn Encryptor> = Box::new(KuznechikEncryptor::new().await?);
+
+        let hashes = ReedSolomonChunksHashes::load_from(&path).await?; // Получаем хэш-суммы из файла
+        let mut chunks = ReedSolomonChunks::recv(hashes).await?; // Получаем чанки по хэшам
+        chunks.decrypt(&decryptor)?; // Расшифровываем чанки
+        chunks.into_file(path).await?; // Восстанавливаем из них содержимое и записываем его в целевой файл
+        Ok(())
+    }
+}
